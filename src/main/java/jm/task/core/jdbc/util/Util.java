26db6d12ff281cba1dc29f7d495b4f238e52eba3
DarkDav <dadasovdavid@gmail.com>
2022-04-19 11:17:24 +0300

    David 1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -18,8 +18,41 @@ public class Util {
     private static String URL = "jdbc:mysql://localhost:3306/fabrica";
     private static String USERNAME = "root";
     private static String PASSWORD = "3We-EP9-ztf-njW";
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
 
-    public static Connection getConnection() {
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/fabrica");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "3We-EP9-ztf-njW");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+                settings.put(Environment.SHOW_SQL, "true");
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+    public static Connection getConnection(){
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
